896180577b9463620aab46ba389bdd0a5b7f1bb7
RafaS92 <manrvaldez..92@gmail.com>
2020-05-22 11:47:24 -0500

    lot missing

--- mod4frontend/src/components/Product.js
+++ mod4frontend/src/components/Product.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
+import Card from "./Card";
 
 export default function Product(props) {
-  return <div>{props.product.description}</div>;
+  return (
+    <div>
+      <Card
+        title={props.product.title}
+        description={props.product.description}
+      />
+    </div>
+  );
 }
