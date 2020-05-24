e503c89adacaefe3d7f5a3409405dad914a5caf1
RafaS92 <manrvaldez..92@gmail.com>
2020-05-23 22:45:50 -0500

    Navbar done

--- mod4frontend/src/App.js
+++ mod4frontend/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Random from "./components/random";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
+      <NavBar />
       <Random />
     </div>
   );
--- mod4frontend/src/components/Product.js
+++ mod4frontend/src/components/Product.js
@@ -1,12 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 import Card from "./Card";
 
 export default function Product(props) {
   let base = props.product.images[0].base_url;
   let second = props.product.images[0].primary;
   let image = base + second;
-  //   let cut = props.product.description;
-  //   console.log(cut.slice(0, 20));
 
   return (
     <div>
