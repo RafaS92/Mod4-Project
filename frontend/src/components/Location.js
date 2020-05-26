71cbfc2adbf9f97c188fdce8482efd895c773fa1
isaac-3 <isaacgc0596@gmail.com>
2020-05-26 09:50:31 -0500

    checks

--- frontend/src/components/Location.js
+++ frontend/src/components/Location.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f8b9ff05d2ebebd404f42145f9b17dc9858e8db834b2ac17a62a7fb399b0372
-size 1792
+oid sha256:31fccac0b34a00739c08ebdf0a17f39787a70f43ab90e1fc4c902f0335d0688b
+size 1856
